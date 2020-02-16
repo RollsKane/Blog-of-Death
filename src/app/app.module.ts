9457d7f5f9d14a7093c5c33de570a64e12e60255
Rolls Kane <rroice@gmail.com>
2020-02-16 11:12:18 +0100

    Formulario casi funcional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -15,7 +15,7 @@ import { DetallePostComponent } from "./detalle-post/detalle-post.component";
     FormularioComponent,
     DetallePostComponent
   ],
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, EventEmitter } from "@angular/core";
 import { SuperServicioService } from "../super-servicio.service";
 import { Router } from "@angular/router";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
@@ -10,26 +10,70 @@ import { FormGroup, FormControl, Validators } from "@angular/forms";
   providers: [SuperServicioService]
 })
 export class FormularioComponent implements OnInit {
+  formulario: FormGroup; // es de tipo FormGroup y se tiene que importar arriba
+
+  formularioEnviado: boolean;
+
+  imagenURL: string;
+  urlEnviada: EventEmitter<string>;
+
   constructor(
     private superServicioService: SuperServicioService,
     private router: Router
-  ) {}
+  ) {
+    this.formularioEnviado = false;
 
-  ngOnInit(): void {}
+    this.formulario = new FormGroup({
+      titulo: new FormControl("", [
+        Validators.required,
+        Validators.maxLength(15), // número de caracteres que yo quiero marcar como máximo
+        Validators.minLength(3)
+      ]),
+      texto: new FormControl("", [
+        Validators.required,
+        Validators.maxLength(140),
+        Validators.minLength(1)
+      ]),
+      autor: new FormControl("", [
+        Validators.required,
+        Validators.maxLength(140),
+        Validators.minLength(1)
+      ]),
+      imagen: new FormControl("", [Validators.required]),
+      fecha: new FormControl("", [
+        Validators.pattern(
+          /^(?:(?:(?:0?[1-9]|1\d|2[0-8])[/](?:0?[1-9]|1[0-2])|(?:29|30)[/](?:0?[13-9]|1[0-2])|31[/](?:0?[13578]|1[02]))[/](?:0{2,3}[1-9]|0{1,2}[1-9]\d|0?[1-9]\d{2}|[1-9]\d{3})|29[/]0?2[/](?:\d{1,2}(?:0[48]|[2468][048]|[13579][26])|(?:0?[48]|[13579][26]|[2468][048])00))$/
+        )
+      ]),
+      categoria: new FormControl("", [Validators.required])
+    });
+
+    this.imagenURL = "";
+
+    this.urlEnviada = new EventEmitter();
+  }
+
+  ngOnInit() {}
 
   manejarClick($event) {
     console.log($event);
+    // tslint:disable-next-line: no-shadowed-variable
+    this.superServicioService.agregarPost($event);
   }
 
-  /*
-  onSubmit(formValues) {
-    console.log(formValues);
+  onSubmit() {
+    console.log(this.formulario.value);
+    this.formularioEnviado = true;
   }
-  */
 
-  /*
-  async manejarClick($event) {
-    this.listaPersonas = this.personasService.getByEdad(30);
-    //this.listaPersonas = await this.personasService.getByEdadPromise(30);
-  }*/
+  emitTarea($event) {
+    console.log($event.target.value);
+
+    this.imagenURL = $event.target.value;
+    this.urlEnviada.emit(this.imagenURL);
+  }
+
+  mostrarImagen() {
+    return this.imagenURL;
+  }
 }
